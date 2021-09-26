21fa9d8ab6103411de078565b3ac1c1d29677f46
blanden-hub <83385242+blanden-hub@users.noreply.github.com>
2021-09-26 10:11:59 -0700

    added models

---
+++ models/book.js
@@ -0,0 +1,34 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const bookSchema = new Schema({
+    title:
+    {
+        type: String,
+        required: true
+    },
+    authors:
+    {
+       type: Array,
+       required: true
+    },
+    description:
+    {
+        type: String,
+        required: true
+    },
+    link:
+    {
+        type: String,
+        required: true
+    },
+    image:
+    {
+        type:String
+    }
+
+});
+
+const Book = mongoose.model("Book", bookSchema);
+
+module.exports = Book;
